66f047a377b2ce58dbcecab449d6bc05130f74d1
Fabian Markert <f.markert87@gmail.com>
2026-01-09 00:21:16 +0100

    spotless apply

--- app/src/main/java/edu/seminar/ouronlyhome/PlanetActivity.kt
+++ app/src/main/java/edu/seminar/ouronlyhome/PlanetActivity.kt
@@ -11,37 +11,39 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class PlanetActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_planet)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContentView(R.layout.activity_planet)
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 
-            val titleResId = intent.getIntExtra(EXTRA_TITLE_ID, R.string.default_title)
-            val backgroundResId = intent.getIntExtra(EXTRA_BACKGROUND_ID, R.drawable.ic_launcher_background)
+      val titleResId = intent.getIntExtra(EXTRA_TITLE_ID, R.string.default_title)
+      val backgroundResId =
+        intent.getIntExtra(EXTRA_BACKGROUND_ID, R.drawable.ic_launcher_background)
 
-            title = getString(titleResId)
-            findViewById<ImageView>(R.id.background_image_view).setImageResource(backgroundResId)
-            findViewById<TextView>(R.id.title_text_view).setText(titleResId)
+      title = getString(titleResId)
+      findViewById<ImageView>(R.id.background_image_view).setImageResource(backgroundResId)
+      findViewById<TextView>(R.id.title_text_view).setText(titleResId)
 
-            insets
-        }
+      insets
     }
+  }
 
-    companion object {
-        // Schlüssel für die Intent-Extras
-        const val EXTRA_TITLE_ID = "extra_title_id"
-        const val EXTRA_BACKGROUND_ID = "extra_background_id"
+  companion object {
+    // Schlüssel für die Intent-Extras
+    const val EXTRA_TITLE_ID = "extra_title_id"
+    const val EXTRA_BACKGROUND_ID = "extra_background_id"
 
-        // Hilfsfunktion zum Starten der Activity
-        fun start(context: Context, titleResId: Int, backgroundResId: Int) {
-            val intent = Intent(context, PlanetActivity::class.java).apply {
-                putExtra(EXTRA_TITLE_ID, titleResId)
-                putExtra(EXTRA_BACKGROUND_ID, backgroundResId)
-            }
-            context.startActivity(intent)
+    // Hilfsfunktion zum Starten der Activity
+    fun start(context: Context, titleResId: Int, backgroundResId: Int) {
+      val intent =
+        Intent(context, PlanetActivity::class.java).apply {
+          putExtra(EXTRA_TITLE_ID, titleResId)
+          putExtra(EXTRA_BACKGROUND_ID, backgroundResId)
         }
+      context.startActivity(intent)
     }
+  }
 }
